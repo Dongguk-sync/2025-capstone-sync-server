16f80b8952d5a3fbaa3efeb38d0da0bd1686a4f7
ryvvoo <rywoo0130@naver.com>
2025-05-22 01:10:08 +0900

    Feat: 사용자 전체 조회 추가

---
+++ baekji/src/main/java/com/baekji/BaekjiApplication.java
@@ -0,0 +1,13 @@
+package com.baekji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaekjiApplication.class, args);
+	}
+
+}
