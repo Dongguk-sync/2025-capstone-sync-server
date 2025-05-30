f522c7067edf97908ec02ce81d51b0290028b557
ryvvoo <rywoo0130@naver.com>
2025-05-30 17:26:39 +0900

    Refactor: AOP를 활요한 Exception handler 추가

--- baekji/src/main/java/com/baekji/user/service/UserService.java
+++ baekji/src/main/java/com/baekji/user/service/UserService.java
@@ -1,33 +1,32 @@
 package com.baekji.user.service;
 
+import com.baekji.common.exception.CommonException;
+import com.baekji.common.exception.ErrorCode;
 import com.baekji.user.domain.UserEntity;
 import com.baekji.user.dto.UserDTO;
+import com.baekji.user.mapper.UserMapper;
 import com.baekji.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
     public List<UserDTO> getAllUsers() {
         List<UserEntity> users = userRepository.findAll();
 
-        return users.stream().map(user -> UserDTO.builder()
-                .userId(user.getUserId())
-                .userEmail(user.getUserEmail())
-                .userPassword(user.getUserPassword()) // 필요시 제거 또는 마스킹
-                .userName(user.getUserName())
-                .userCreatedAt(user.getUserCreatedAt())
-                .userProfileUrl(user.getUserProfileUrl())
-                .userPhoneNumber(user.getUserPhoneNumber())
-                .userNickname(user.getUserNickname())
-                .build()
-        ).collect(Collectors.toList());
+        if (users.isEmpty()) {
+            throw new CommonException(ErrorCode.NOT_FOUND_USER);
+        }
+
+        return userMapper.toUserDTOList(users);
     }
+
 }
