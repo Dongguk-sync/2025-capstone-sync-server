09991c1e5ab31b463bee22f9271caa8d6666568e
ryvvoo <rywoo0130@naver.com>
2025-05-31 16:37:08 +0900

    Feat: 사용자 정보 조회 API추가

---
+++ baekji/src/main/java/com/baekji/config/AppConfiguration.java
@@ -0,0 +1,44 @@
+package com.baekji.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class AppConfiguration {
+
+
+//    /* 설명. Security 자체에서 사용할 암호화 방식용 bean 추가 */
+//    @Bean
+//    public BCryptPasswordEncoder getBCryptPasswordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+
+    /* 설명. RestTemplate 추가 */
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    /* 설명. CORS 설정 추가 */
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+
+        configuration.setAllowedOrigins(List.of("http://localhost:5173/")); // Allow frontend
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*")); // Allow all headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
---
+++ baekji/src/main/java/com/baekji/config/S3Config.java
@@ -0,0 +1,62 @@
+//package com.baekji.config;
+//
+//import com.amazonaws.auth.AWSCredentials;
+//import com.amazonaws.auth.AWSStaticCredentialsProvider;
+//import com.amazonaws.auth.BasicAWSCredentials;
+//import com.amazonaws.services.s3.AmazonS3Client;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//public class S3Config {
+//    @Value("${cloud.aws.credentials.access-key}")
+//    private String accessKey;
+//
+//    @Value("${cloud.aws.credentials.secret-key}")
+//    private String accessSecret;
+//
+//    @Value("${cloud.aws.region.static}")
+//    private String region;
+//
+//    @Value("${cloud.aws.s3.buckets.inflow-emp-profile}")
+//    private String inflowEmpProfileBucket;
+//
+//    @Value("${cloud.aws.s3.buckets.inflow-contract}")
+//    private String inflowContractBucket;
+//
+//    @Value("${cloud.aws.s3.buckets.inflow-vacation-attendance}")
+//    private String inflowVacationAttendanceBucket;
+//
+//    @Value("${cloud.aws.s3.buckets.inflow-excel-form}")
+//    private String inflowExcelFormBucket;
+//
+//    @Bean
+//    public AmazonS3Client s3Client() {
+//        AWSCredentials credentials = new BasicAWSCredentials(accessKey, accessSecret);
+//        return (AmazonS3Client) AmazonS3Client.builder()
+//                .withRegion(region)
+//                .withCredentials(new AWSStaticCredentialsProvider(credentials))
+//                .build();
+//    }
+//
+//    //설명. 버킷들 이름 Getters
+//    //설명.1. 사원별 프로필 버킷
+//    public String getInflowEmpProfileBucket() {
+//        return inflowEmpProfileBucket;
+//    }
+//
+//    //설명.2. 사원 계약서 버킷
+//    public String getInflowContractBucket() {
+//        return inflowContractBucket;
+//    }
+//
+//    //설명.3. 근태 및 휴가 버킷
+//    public String getInflowVacationAttendanceBucket() {
+//        return inflowVacationAttendanceBucket;
+//    }
+//
+//    //설명.4. 엑셀 양식 버킷
+//    public String getInflowExcelFormBucket() {return inflowExcelFormBucket;}
+//
+//}
