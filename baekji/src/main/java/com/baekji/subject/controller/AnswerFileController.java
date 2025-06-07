dd3124a2e87c9ec298cdfd830c6ed363dc863e6a
ryvvoo <rywoo0130@naver.com>
2025-06-07 19:54:57 +0900

    Feat: 교안등록 API 추가

--- baekji/src/main/java/com/baekji/subject/controller/AnswerFileController.java
+++ baekji/src/main/java/com/baekji/subject/controller/AnswerFileController.java
@@ -5,6 +5,7 @@ import com.baekji.subject.dto.AnswerFileDTO;
 import com.baekji.subject.service.AnswerFileService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -15,7 +16,6 @@ public class AnswerFileController {
 
     private final AnswerFileService answerFileService;
 
-    // 설명.1.1. 과목별 교안 전체 조회
     // 설명.1.1. 과목별 교안 전체 조회
     @GetMapping("/subject-id/{subjectId}")
     public ResponseDTO<List<AnswerFileDTO>> getAnswerFilesBySubjectId(
@@ -47,6 +47,20 @@ public class AnswerFileController {
         List<AnswerFileDTO> response = answerFileService.getAnswerFilesByUserIdAndFileName(userId, fileName);
         return ResponseDTO.ok(response);
     }
+
+    //설명.2. 교안 등록
+    @PostMapping("/upload")
+    public ResponseDTO<AnswerFileDTO> uploadAnswerFile(
+            @RequestParam("subject_id") Long subjectId,
+            @RequestParam("file") MultipartFile file) {
+
+        AnswerFileDTO response = answerFileService.saveAnswerFile(subjectId, file);
+        return ResponseDTO.ok(response);
+    }
+
+    //설명.3. 교안 삭제
+
+
 
 
 }
--- baekji/src/main/java/com/baekji/subject/repository/AnswerFileRepository.java
+++ baekji/src/main/java/com/baekji/subject/repository/AnswerFileRepository.java
@@ -1,6 +1,7 @@
 package com.baekji.subject.repository;
 
 import com.baekji.subject.domain.AnswerFile;
+import com.baekji.subject.domain.Subject;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -13,4 +14,9 @@ public interface AnswerFileRepository extends JpaRepository<AnswerFile, Long> {
     List<AnswerFile> findBySubjectUserUserId(Long userId);
 
     // 사용자별 교안명 검색
-    List<AnswerFile> findBySubjectUserUserIdAndFileNameContaining(Long userId, String fileName);}
+    List<AnswerFile> findBySubjectUserUserIdAndFileNameContaining(Long userId, String fileName);
+
+    // 개수 세기
+    int countBySubject(Subject subject);
+
+}
